fb228c15afe0755d6ea7c1ca159e1475749d88fa
Andrea-Crowchild <noisysunset@gmail.com>
2026-05-10 17:40:42 -0400

    Got new files working

--- hexgrim.py
+++ hexgrim.py
@@ -11,20 +11,28 @@ def cli(ctx):
         print("Dummy behavior")
 
 
-@cli.command
+@cli.command()
 def new():
-    config_dir = os.path.expanduser("~/Code/python/hexgrim/")
+    config_dir = os.path.expanduser("~/Code/python/hexgrim/config/")
     config_file = os.path.join(config_dir, "hexgrim.toml")
     os.makedirs(config_dir, exist_ok=True)
 
     if os.path.exists(config_file):
-        print("Are you sure you want to delete all data? y/n")
-        choice = input()
-        if choice == "y":
-            with open(config_file, "w") as f:
-                f.write("")
-        else:
-            print("Data retained")
+        while True:
+            print("Are you sure you wish to reset saved data?")
+            print("Type Y/N to continue")
+            choice = input()
+            if choice == "Y" or "y":
+                with open(config_file, "w") as f:
+                    tomlkit.dump({"commands": {}}, f)
+                print("Saved data cleared!")
+                break
+            else:
+                print("Saved data retained")
+                break
+    else:
+        with open(config_file, "w") as f:
+            tomlkit.dump({"commands": {}}, f)
 
 
 if __name__ == "__main__":

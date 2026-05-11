ac4412f07ac23b5fa0b26e2d9a3247d580b889bb
Andrea-Crowchild <noisysunset@gmail.com>
2026-05-10 20:12:55 -0400

    moved config directory to ~/.config/hexgrim/ , added remove feature

--- hexgrim.py
+++ hexgrim.py
@@ -3,12 +3,15 @@ import os
 import click
 import tomlkit
 
+config_dir = os.path.expanduser("~/.config/hexgrim/")
+CONFIG_FILE = os.path.join(config_dir, "hexgrim.toml")
+
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
     if ctx.invoked_subcommand is None:
-        with open("/home/hexbeak/Code/python/hexgrim/config/hexgrim.toml", "r") as f:
+        with open(CONFIG_FILE, "r") as f:
             doc = tomlkit.load(f)
 
         for name, desc in sorted(doc["commands"].items()):
@@ -19,33 +22,31 @@ def cli(ctx):
 @click.argument("name")
 @click.argument("description")
 def add(name, description):
-    with open("/home/hexbeak/Code/python/hexgrim/config/hexgrim.toml", "r") as f:
+    with open(CONFIG_FILE, "r") as f:
         doc = tomlkit.load(f)
     try:
         entry = tomlkit.table()
         entry.add("description", description)
 
         doc["commands"].add(name, entry)
+        with open(CONFIG_FILE, "w") as f:
+            tomlkit.dump(doc, f)
+        print("Entry added to grimoire")
     except:
         print("That's already in the grimoire!")
-
-    with open("/home/hexbeak/Code/python/hexgrim/config/hexgrim.toml", "w") as f:
-        tomlkit.dump(doc, f)
 
 
 @cli.command()
 def new():
-    config_dir = os.path.expanduser("~/Code/python/hexgrim/config/")
-    config_file = os.path.join(config_dir, "hexgrim.toml")
     os.makedirs(config_dir, exist_ok=True)
 
-    if os.path.exists(config_file):
+    if os.path.exists(CONFIG_FILE):
         while True:
             print("Are you sure you wish to reset saved data?")
             print("Type Y/N to continue")
             choice = input()
             if choice == "Y" or choice == "y":
-                with open(config_file, "w") as f:
+                with open(CONFIG_FILE, "w") as f:
                     tomlkit.dump({"commands": {}}, f)
                 print("Saved data cleared!")
                 break
@@ -53,14 +54,27 @@ def new():
                 print("Saved data retained")
                 break
     else:
-        with open(config_file, "w") as f:
+        with open(CONFIG_FILE, "w") as f:
             tomlkit.dump({"commands": {}}, f)
 
 
 @cli.command()
 @click.argument("name")
 def remove(name):
-    pass
+
+    try:
+        with open(CONFIG_FILE, "r") as f:
+            doc = tomlkit.load(f)
+    except:
+        print("Unable to read grimoire!")
+
+    if name in doc["commands"]:
+        doc["commands"].pop(name)
+    else:
+        print(f"{name} not found in grimoire!")
+
+    with open(CONFIG_FILE, "w") as f:
+        tomlkit.dump(doc, f)
 
 
 if __name__ == "__main__":
